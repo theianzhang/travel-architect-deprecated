f82dc119d72629bfb39750ef975f6ab1ffa41af5
Ian Zhang <ian.zhang@hitachivantara.com>
2017-10-10 22:07:59 -0700

    Initial Commit

---
+++ Travel Architect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travel Architect
+//
+//  Created by Ian Zhang on 10/10/17.
+//  Copyright © 2017 Ian Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
