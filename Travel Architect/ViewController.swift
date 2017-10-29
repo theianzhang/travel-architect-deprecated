47bbaf13511e42c99c8c1dded644db071cce1e93
Ian Zhang <ian.chen.zhang@gmail.com>
2017-10-29 14:32:45 -0700

    update check

--- Travel Architect/ViewController.swift
+++ Travel Architect/ViewController.swift
@@ -9,34 +9,69 @@
 import UIKit
 import Alamofire
 import AlamofireImage
+import CoreLocation
 
-class ViewController: UIViewController
+class ViewController: UIViewController, CLLocationManagerDelegate
 {
 
     //MARK: properties
     @IBOutlet weak var coordinatesLabel: UILabel!
     @IBOutlet weak var showStructures: UIImageView!
     
+    // Used to start getting the users location
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        Alamofire.request("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=Seattle&exintro=1").responseJSON { response in
-//            print("Request: \(String(describing: response.request))")   // original url request
-//            print("Response: \(String(describing: response.response))") // http url response
-//            print("Result: \(response.result)")                         // response serialization result
-//
-//            if let json = response.result.value {
-//                print("JSON: \(json)") // serialized json response
-//            }
-//
-//            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-//                print("Data: \(utf8Text)") // original server data as UTF8 string
-//            }
-//        }
+        // For use when the app is open & in the background
+        locationManager.requestAlwaysAuthorization()
         
+        // For use when the app is open
+        //locationManager.requestWhenInUseAuthorization()
+        
+        // If location services is enabled get the users location
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuary here.
+            locationManager.startUpdatingLocation()
+        }
+        
+        getCoordinates()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getCoordinates()
+    {
+        
+    }
+    
+    func findStructures()
+    {
+                Alamofire.request("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=Seattle&exintro=1").responseJSON { response in
+                    print("Request: \(String(describing: response.request))")   // original url request
+                    print("Response: \(String(describing: response.response))") // http url response
+                    print("Result: \(response.result)")                         // response serialization result
+        
+                    if let json = response.result.value {
+                        print("JSON: \(json)") // serialized json response
+                    }
+        
+                    if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                        print("Data: \(utf8Text)") // original server data as UTF8 string
+                    }
+                }
+    }
+    
+    func getImages() -> Void
+    {
         Alamofire.request("https://upload.wikimedia.org/wikipedia/commons/3/36/SeattleI5Skyline.jpg", method: .get).responseImage { response in
             guard let image = response.result.value else {
                 // Handle error
@@ -45,10 +80,38 @@ class ViewController: UIViewController
             self.showStructures.image = image
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Print out the location to the console
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print(location.coordinate)
+        }
+    }
+    
+    // If we have been deined access give the user the option to change it
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if(status == CLAuthorizationStatus.denied) {
+            showLocationDisabledPopUp()
+        }
+    }
+    
+    // Show the popup to the user if we have been deined access
+    func showLocationDisabledPopUp() {
+        let alertController = UIAlertController(title: "Background Location Access Disabled",
+                                                message: "In order to deliver pizza we need your location",
+                                                preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let openAction = UIAlertAction(title: "Open Settings", style: .default) { (action) in
+            if let url = URL(string: UIApplicationOpenSettingsURLString) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        alertController.addAction(openAction)
+        
+        self.present(alertController, animated: true, completion: nil)
     }
 
     //MARK: actions
